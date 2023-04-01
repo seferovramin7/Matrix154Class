c31bfcd5bce34d14cb835bc3b0638d12ec9f7e50
safarovrag <ramin.safarov@kapitalbank.az>
2023-04-01 15:41:49 +0400

    #2 Lesson

---
+++ src/main/java/org/lessons/Lesson_2/ClassWork.java
@@ -0,0 +1,30 @@
+package org.lessons.Lesson_2;
+
+public class ClassWork {
+    int ageOfTheChild = 190; // global deyishen
+
+    public static void main(String[] args) {
+//        System.out.println("Zehmet olmasa adinizi daxil edin");
+//        Scanner scanner = new Scanner(System.in);
+//        int g = scanner.nextInt();
+
+        String name = "Rauf";
+        char r = 'R';
+        char a = 'A';
+        char u = 'U';
+        char f = 'F';
+//        System.out.println(r+a+u+f);
+//        System.out.println(a);
+
+        boolean isClicked = true;
+
+
+        int t = 20;
+        if (t % 2 == 0) {
+//            System.out.println("Cut reqem");
+        }
+
+        System.out.println(5/2);
+
+    }
+}
